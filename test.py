46aca60923d3082fe8b33aa909fcf553b3a333e3
Yasushi Iwaki <you@example.com>
2023-11-21 20:01:21 +0900

    modified:   test.ipynb 	modified:   test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from sudoku import c_brute_force
 
 filepath = "data/evil_3.csv"
@@ -6,7 +7,12 @@ x = np.loadtxt(filepath, delimiter=",").astype(np.uint16)
 print("Puzzle:")
 print(x)
 
+start = time.process_time()
 y = c_brute_force(x)
+end = time.process_time()
+elapsed = end - start
 
 print("Solution:")
 print(y)
+
+print(f"elapsed time: {elapsed:.6f}")
