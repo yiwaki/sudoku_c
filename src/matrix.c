73046d298e0aca166ab5ed11ce4a36071ddaa073
Yasushi Iwaki <yasushi.iwaki@gmail.com>
2023-12-31 10:07:32 +0900

    modified:   src/matrix.c 	modified:   src/sudoku.c

--- src/matrix.c
+++ src/matrix.c
@@ -24,50 +24,50 @@ void cell_no_to_addr(const int cell_no, address_t *const addr) {
 int addr_to_block_no(const block_t block_type, const address_t *const addr) {
     int block_no;
     switch (block_type) {
-        case Row:
-            block_no = addr->row;
-            break;
+    case Row:
+        block_no = addr->row;
+        break;
 
-        case Column:
-            block_no = addr->col;
-            break;
+    case Column:
+        block_no = addr->col;
+        break;
 
-        case Square:
-            block_no = addr->row / SQUARE_SIZE * SQUARE_SIZE + addr->col / SQUARE_SIZE;
-            break;
+    case Square:
+        block_no = addr->row / SQUARE_SIZE * SQUARE_SIZE + addr->col / SQUARE_SIZE;
+        break;
 
-        default:
-            block_no = -1;
-            break;
+    default:
+        block_no = -1;
+        break;
     }
     return block_no;
 }
 
 void block_range(const block_t block_type, const int block_no, int row_range[2], int col_range[2]) {
     switch (block_type) {
-        case Row:
-            row_range[0] = block_no;
-            row_range[1] = row_range[0] + 1;
-            col_range[0] = 0;
-            col_range[1] = MATRIX_SIZE;
-            break;
+    case Row:
+        row_range[0] = block_no;
+        row_range[1] = row_range[0] + 1;
+        col_range[0] = 0;
+        col_range[1] = MATRIX_SIZE;
+        break;
 
-        case Column:
-            row_range[0] = 0;
-            row_range[1] = MATRIX_SIZE;
-            col_range[0] = block_no;
-            col_range[1] = col_range[0] + 1;
-            break;
+    case Column:
+        row_range[0] = 0;
+        row_range[1] = MATRIX_SIZE;
+        col_range[0] = block_no;
+        col_range[1] = col_range[0] + 1;
+        break;
 
-        case Square:
-            row_range[0] = block_no / SQUARE_SIZE * SQUARE_SIZE;
-            row_range[1] = row_range[0] + SQUARE_SIZE;
-            col_range[0] = block_no % SQUARE_SIZE * SQUARE_SIZE;
-            col_range[1] = col_range[0] + SQUARE_SIZE;
-            break;
+    case Square:
+        row_range[0] = block_no / SQUARE_SIZE * SQUARE_SIZE;
+        row_range[1] = row_range[0] + SQUARE_SIZE;
+        col_range[0] = block_no % SQUARE_SIZE * SQUARE_SIZE;
+        col_range[1] = col_range[0] + SQUARE_SIZE;
+        break;
 
-        default:
-            // noop
-            break;
+    default:
+        // noop
+        break;
     }
 }
