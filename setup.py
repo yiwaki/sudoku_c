25da74188974ce67867a63b9cbcb18662e8f82cc
Yasushi Iwaki <yasushi.iwaki@gmail.com>
2026-05-07 11:34:24 +0900

    mac対応

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
-from setuptools import setup, Extension, find_packages
-import pybind11
+import platform
 
+import pybind11  # pyright: ignore[reportMissingImports]
+from setuptools import (  # pyright: ignore[reportMissingModuleSource]
+    Extension,
+    find_packages,
+    setup,
+)
+
+extra_args = []
+if platform.system() == "Windows":
+    # MSVC用の設定
+    extra_args += ["/wd4819", "/O2", "/utf-8"]
+else:
+    # Mac/Linux (GCC/Clang) 用の設定
+    extra_args += ["-O3"]
 
 # すべてのCファイルをコンパイル対象に含める
 ext_modules = [
@@ -10,17 +23,16 @@ ext_modules = [
             "src/sudoku_c/bridge.cpp",
             "src/sudoku_c/sudoku.c",
             "src/sudoku_c/matrix.c",
-            "src/sudoku_c/bitmap.c"
+            "src/sudoku_c/bitmap.c",
         ],
         include_dirs=[
             pybind11.get_include(),
             "src/sudoku_c",
         ],
-        language='c++',
-        extra_compile_args=['/wd4819', '/O2', '/utf-8'],
+        language="c++",
+        extra_compile_args=extra_args,
     ),
 ]
-
 
 setup(
     name="sudoku_c",
