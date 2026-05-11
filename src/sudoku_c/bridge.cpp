de40b6ed750f13e75ccc1c39d06e13f641c7c0cc
Yasushi Iwaki <yasushi.iwaki@gmail.com>
2026-05-11 20:34:25 +0900

    modified:   src/sudoku_c/bridge.cpp 	modified:   src/sudoku_c/sudoku.c

--- src/sudoku_c/bridge.cpp
+++ src/sudoku_c/bridge.cpp
@@ -1,6 +1,5 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
-#include <string.h>
 
 extern "C" {
 #include "sudoku.h"
--- src/sudoku_c/sudoku.c
+++ src/sudoku_c/sudoku.c
@@ -1,8 +1,6 @@
 #include "sudoku.h"
 
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "bitmap.h"
